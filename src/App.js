156345aaa02732044c4772765709691296b076bd
Neeraj Gupta <neerajgupta@192.168.1.4>
2021-05-24 10:38:54 +0530

    added spotify auth

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar/Sidebar.jsx";
 import Content from "./components/Content/Content";
 import BottomNav from "./components/BottomNav/BottomNav.jsx";
 import Login from "./components/pages/Login/Login";
+import { SpotifyApiContext } from "react-spotify-api";
+import Cookies from "js-cookie";
+// import "react-spotify-auth/dist/index.css";
 
 function App() {
+  const token = Cookies.get("spotifyAuthToken");
   return (
     <div className="app">
-      <Sidebar />
-      <Content />
-      <BottomNav />
-      {/* <Login /> */}
+      {/* <Sidebar /> */}
+      {/* <Content /> */}
+      {/* <BottomNav /> */}
+
+      {token ? (
+        <SpotifyApiContext.Provider value={token}>
+          {/* Your Spotify Code here */}
+          <Sidebar />
+          <Content />
+          <BottomNav />
+        </SpotifyApiContext.Provider>
+      ) : (
+        // Display the login page
+        <Login />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React from "react";
 import "./Login.css";
-import image from '../../../assets/spotify_baner.png';
+import image from "../../../assets/spotify_baner.png";
+import { SpotifyAuth, Scopes } from "react-spotify-auth";
 
 function Login() {
-    return (
-        <div className="login">
-            <img src={image} alt="spotify" className="login__image"/>
-            <button className="login__button">Login</button>
-        </div>
-    )
+  return (
+    <div className="login">
+      <img src={image} alt="spotify" className="login__image" />
+      {/* <button className="login__button"> */}
+        <SpotifyAuth
+          redirectUri="http://localhost:3000/callback"
+          clientID="42b4de6c994644ffbe33fde0d2bc8bf4"
+          scopes={[Scopes.userReadPrivate, "user-read-email"]} // either style will work
+          btnClassName="login__button"
+          showDialog={true}
+          noLogo={true}
+        />
+      {/* </button> */}
+    </div>
+  );
 }
 
-export default Login
+export default Login;
